524adc894dbdd272963dd0a22c6a70a8e91524cd
Jeremy Parker <jrobinparker@gmail.com>
2018-06-19 20:08:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,16 @@
 class Application
   
-  
+    def call(env)
+    resp = Rack::Response.new
+    
+    if Time.now < 12:00 
+      resp.write "Good Morning!"
+    elsif Time.now > 12:00
+      resp.write "Good Afternoon!"
+    end 
+
+    
+    resp.finish
+  end
+
+end
